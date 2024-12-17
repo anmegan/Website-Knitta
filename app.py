a3def1a8bc3bd615c3a061bb267aa86b4b9d06bb
Prita <146433531+PritaHesmono@users.noreply.github.com>
2024-12-17 10:28:45 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,47 @@
+from flask import Flask, request, render_template
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-import pickle
+import joblib
+from sklearn.ensemble import RandomForestRegressor
 
-# Load the csv file
-df = pd.read_csv("Data.csv")
+app = Flask(__name__)
 
-print(df.head())
+# Muat model yang sudah dilatih
+model = joblib.load('app/models/model.pkl')
 
-# Select independent and dependent variable
-X = df[["Kode_Provinsi", "Nama_Provinsi", "Kode_Kabupaten_Kota", "Nama_Kabupaten_Kota", "Jumlah_Balita_Stunting", "Satuan"]]
-y = df["Tahun"]
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Split the dataset into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=50)
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Mengambil file CSV yang di-upload
+    file = request.files['file']
+    if file:
+        # Membaca CSV yang di-upload
+        data = pd.read_csv(file)
+        
+        # Pra-pemrosesan data jika diperlukan
+        # Asumsi data sudah sesuai dengan format yang diinginkan
+        X = data.drop(columns=['target'])  # Menghapus kolom target jika ada
+        predictions = model.predict(X)  # Menggunakan model untuk prediksi
 
-# Feature scaling
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test= sc.transform(X_test)
+        # Menyiapkan hasil prediksi
+        result = []
+        for i, year in enumerate(data['tahun']):
+            predicted_year = year + 1  # Prediksi untuk tahun berikutnya
+            predicted_percentage = predictions[i]  # Persentase prediksi stunting
+            
+            # Menambahkan hasil prediksi ke dalam list
+            result.append({
+                'tahun': year,
+                'predicted_year': predicted_year,
+                'predicted_percentage': round(predicted_percentage, 2)
+            })
+        
+        # Menyampaikan hasil prediksi ke template
+        return render_template('index.html', result=result)
 
-# Instantiate the model
-classifier = RandomForestClassifier()
+    return "File tidak valid atau tidak ditemukan"
 
-# Fit the model
-classifier.fit(X_train, y_train)
-
-# Make pickle file of our model
-pickle.dump(classifier, open("model.pkl", "wb"))
+if __name__ == '__main__':
+    app.run(debug=True)
